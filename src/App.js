4ce979224a0174ef2143ea997c96d0281720eee6
ffznrr <ffznrr@gmail.com>
2024-06-06 14:02:52 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,46 +1,82 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      action: "homework progate",
-    },
-    {
-      id: 2,
-      action: "homework progate 2",
-    },
-  ]);
+  const initialTodos = JSON.parse(localStorage.getItem("list")) || [];
+  const [todos, setTodos] = useState(initialTodos);
+  const [coret, setCoret] = useState(false);
+  const [isi, setIsi] = useState();
 
-  console.log(todos);
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(todos));
+  }, [todos]);
 
   const handle = (event) => {
     event.preventDefault();
     const isi = event.target.elements.input.value;
-
     setTodos([...todos, { id: todos.length + 1, action: isi }]);
     event.target.reset();
   };
-  return (
-    <div>
-      <h1>To Do List App</h1>
 
+  const handleDel = (id) => {
+    const storedlist = JSON.parse(localStorage.getItem("list") || []);
+    const filtering = storedlist.filter((data) => data.id !== id);
+    setTodos(filtering);
+  };
+
+  const hide = (data) => {
+    setCoret(!coret);
+    setIsi(data.id);
+  };
+
+  return (
+    <div className="text-center">
+      <h1>To Do List App</h1>
       <div>
         <form onSubmit={handle}>
-          <label>INPUT</label>
+          <label className="mr-2">Input</label>
           <input
+            className=" mr-2 border-2 rounded-xl p-2"
             type="text"
             name="input"
           />
-          <button>Tambah</button>
+          <button className="p-2 bg-lime-300 rounded-xl">Tambah</button>
         </form>
       </div>
-
-      <ul>
-        {todos.map((data) => (
-          <li key={data.id}>{data.action}</li>
-        ))}
-      </ul>
+      <div className="flex justify-center m-5">
+        <table class="border-collapse border border-slate-400">
+          <thead>
+            <tr>
+              <th class="border border-slate-300 px-5 py-2">No.</th>
+              <th class="border border-slate-300 px-5 py-2">To Do List</th>
+              <th class="border border-slate-300 px-5 py-2">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((data, index) => (
+              <tr key={index}>
+                <td className="border border-slate-300 px-5 py-2">
+                  {index + 1}
+                </td>
+                <td className="border border-slate-300 px-5 py-2">
+                  <span
+                    onClick={() => hide(data)}
+                    className={data.id === isi && coret ? "line-through" : ""}
+                  >
+                    {data.action}
+                  </span>
+                </td>
+                <td className="border border-slate-300 px-5 py-2">
+                  <img
+                    src="/delete.png"
+                    alt="delete"
+                    onClick={() => handleDel(data.id)}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
